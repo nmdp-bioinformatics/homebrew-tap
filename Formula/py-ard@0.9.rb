b24865a01df6cf03209969c1a02b9acf08fa5732
pbashyal-nmdp <pbashyal@nmdp.org>
2023-02-16 12:18:07 -0600

    Added brew test. Asserts that HLA-A*01:01:01 can be reduced

--- Formula/py-ard@0.9.rb
+++ Formula/py-ard@0.9.rb
@@ -48,15 +48,8 @@ class PyArdAT09 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test py-ard@0.9`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    # Do a basic test of reducing HLA-A*01:01:01
+    redux_result = shell_output("#{bin}/pyard --gl 'HLA-A*01:01:01' -r lgx")
+    assert_match('HLA-A*01:01', redux_result.strip)
   end
 end
