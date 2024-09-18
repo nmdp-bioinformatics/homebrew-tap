4bad77f638e4eb432bb67eff25c9af5dbad51bd6
Pradeep Bashyal <pbashyal@nmdp.org>
2024-09-18 15:10:55 -0500

    Upgrade py-ard to 1.5.1 and pandas/numpy to 2.x.x

---
+++ Formula/py-ard@1.5.rb
@@ -0,0 +1,51 @@
+class PyArd < Formula
+  include Language::Python::Virtualenv
+
+  desc "Project py-ard is the swiss army knife of HLA ARD reductions in Python"
+  homepage "https://py-ard.org"
+  url "https://files.pythonhosted.org/packages/c3/a8/91f2346df41644f6e7a380cdd35322eb17088d51ddb6d4ebf2bd38b111b6/py-ard-1.5.1.tar.gz"
+  sha256 "dba86f59d63184f024f2398e9e60f6b17ad1befe38722914bc8ead1f6fd483d9"
+  license "LGPL-3.0"
+
+  depends_on "python@3.12"
+
+  resource "numpy" do
+    url "https://files.pythonhosted.org/packages/a9/75/10dd1f8116a8b796cb2c737b674e02d02e80454bda953fa7e65d8c12b016/numpy-2.0.2.tar.gz"
+    sha256 "883c987dee1880e2a864ab0dc9892292582510604156762362d9326444636e78"
+  end
+
+  resource "pandas" do
+    url "https://files.pythonhosted.org/packages/88/d9/ecf715f34c73ccb1d8ceb82fc01cd1028a65a5f6dbc57bfa6ea155119058/pandas-2.2.2.tar.gz"
+    sha256 "9e79019aba43cb4fda9e4d983f8e88ca0373adbb697ae9c6c43093218de28b54"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/66/c0/0c8b6ad9f17a802ee498c46e004a0eb49bc148f2fd230864601a86dcf6db/python-dateutil-2.9.0.post0.tar.gz"
+    sha256 "37dd54208da7e1cd875388217d5e00ebd4179249f90fb72437e91a35459a0ad3"
+  end
+
+  resource "pytz" do
+    url "https://files.pythonhosted.org/packages/3a/31/3c70bf7603cc2dca0f19bdc53b4537a797747a58875b552c8c413d963a3f/pytz-2024.2.tar.gz"
+    sha256 "2aa355083c50a0f93fa581709deac0c9ad65cca8a9e9beac660adcbd493c798a"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+  end
+
+  resource "toml" do
+    url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
+    sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    # Do a basic test of reducing HLA-A*01:01:01
+    redux_result = shell_output("#{bin}/pyard --gl 'HLA-A*01:01:01' -r lgx")
+    assert_match("HLA-A*01:01", redux_result.strip)
+  end
+end
